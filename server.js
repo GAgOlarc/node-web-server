5858532957d811b5d38e7c0847b1134db0cf34b5
Andras Nett <andras.nett@gmail.com>
2018-05-19 11:06:48 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(now);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome on the Website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send('<h1>Unable to handle request</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port 3000 ${port}`);
+});
